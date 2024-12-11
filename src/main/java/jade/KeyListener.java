1b5490c2a98e05f4ea1470f38d1318484ec5408b
m4l0n6 <mahonglonghg123@gmail.com>
2024-12-11 16:34:13 +0700

    Fix scence, add physic, add copy obj, fix z-index

--- src/main/java/jade/KeyListener.java
+++ src/main/java/jade/KeyListener.java
@@ -1,11 +1,11 @@
 package jade;
 
-import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyListener {
     private static KeyListener instance;
     private boolean keyPressed[] = new boolean[350];
+    private boolean keyBeginPress[] = new boolean[350];
 
     private KeyListener() {
 
@@ -22,12 +22,22 @@ public class KeyListener {
     public static void keyCallback(long window, int key, int scancode, int action, int mods) {
         if (action == GLFW_PRESS) {
             get().keyPressed[key] = true;
+            get().keyBeginPress[key] = true;
         } else if (action == GLFW_RELEASE) {
             get().keyPressed[key] = false;
+            get().keyBeginPress[key] = false;
         }
     }
 
     public static boolean isKeyPressed(int keyCode) {
         return get().keyPressed[keyCode];
+    }
+
+    public static boolean keyBeginPress(int keyCode){
+        boolean result = get().keyBeginPress[keyCode];
+        if(result){
+            get().keyBeginPress[keyCode] = false;
+        }
+        return result;
     }
 }
